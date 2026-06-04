2d847febaf0e6355b2006cc617759488f86540eb
andrecastro0o <andre.castro@dans.knaw.nl>
2026-06-04 11:35:33 +0200

    schemas/Hierarchy-Preferred-Formats.linkml.yaml schemas/pff-src.linkml.yaml

--- linkml-csv2rdf.sh
+++ linkml-csv2rdf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # convert CSV to RDF using LinkML schema and generator
 
-linkml-convert --schema schemas/Hierarchy-Preferred-Formats.linkml.yaml -t rdf pff-src-SAMPLE.csv
+linkml-convert --schema schemas/pff-src.linkml.yaml -t rdf pff-src-SAMPLE.csv
