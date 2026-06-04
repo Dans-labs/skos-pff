ec0204ff0400224c406b105b4810928c3d1add2d
andrecastro0o <andre.castro@dans.knaw.nl>
2026-06-04 14:59:19 +0200

    WIP: SPARQL construct onto Skos

--- linkml-csv2rdf.sh
+++ linkml-csv2rdf.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # convert CSV to RDF using LinkML schema and generator
 
-linkml-convert --schema schemas/pff-src.linkml.yaml -t rdf pff-src-SAMPLE.csv
+INPUT_DATA=pff-src-SAMPLE.csv
+SCHEMA=schemas/pff-src.linkml.yaml
+INPUT_DATA_rdf=pff-src-SAMPLE.ttl
+
+
+linkml-convert --schema $SCHEMA -t ttl $INPUT_DATA -o $INPUT_DATA_rdf
